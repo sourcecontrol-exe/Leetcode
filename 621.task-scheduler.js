ef64640266fe4ab236a5bbd0d459c0506c91eaf7
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-05 05:30:27 +0530

    ar

--- 621.task-scheduler.js
+++ 621.task-scheduler.js
@@ -10,8 +10,38 @@
  * @param {number} n
  * @return {number}
  */
-var leastInterval = function(tasks, n) {
-    console.log(3)
+var leastInterval = function (tasks, n) {
+   if (n === 0) return tasks.length;
+
+  const map = {};
+  for (let t of tasks) {
+    if (map[t] == null) map[t] = 0;
+    map[t]++;
+  }
+
+  let max = 0;  // task max frequency
+  for (let t in map) {
+    max = Math.max(max, map[t]);
+  }
+
+  let count = 0;  // the number of tasks with same max frequency
+  for (let t in map) {
+    if (map[t] === max) {
+      count++;
+    }
+  }
+
+  return Math.max(
+    // case 1
+    // AB...AB... = (max - 1) * (n + 1)
+    // AB...AB...AB = (max - 1) * (n + 1) + count
+    (max - 1) * (n + 1) + count,
+
+    // case 2
+    // e.g. (ABC)(ABD)(ABEF)
+    tasks.length,
+  ); 
+
 };
 // @lc code=end
 
