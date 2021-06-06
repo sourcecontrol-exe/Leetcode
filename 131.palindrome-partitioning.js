305d9412fe8a3b278e9b4316b87a988d1b8b91b6
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-06 19:10:44 +0530

    leetcode problem

--- 131.palindrome-partitioning.js
+++ 131.palindrome-partitioning.js
@@ -15,33 +15,31 @@ var palindrome = (str) => {
     let right = str.length - 1;
 
     while (left < right) {
-        if (str[left] == str[right]) return false;
+        if (str[left] !== str[right]) return false;
         left++;
         right--;
     }
     return true;
 }
 
-const result = []
-
-function permutate(s, arr = []) {
-
-    if(!s.length) result.push(arr)
-
-    for (var i = 1; i <=s.length; i++){
-        if(palindrome(s.slice(0,i))){
-            console.log(s.slice(0,i))
-            permutate(s.slice(i), [...arr, s.slice(0,i)])
-        }        
-    }
-}
 var partition = function (s) {
+    let result = [];
+    function permutate(s, arr = []) {
 
+        if (s.length == 1) {
+            arr.push(s)
+            result.push(arr)
+        }
+        for (var i = 1; i < s.length; i++) {
+            if (palindrome(s.slice(0, i))) {
+                permutate(s.slice(i), [...arr, s.slice(0, i)])
+            }
+        }
+    }
     permutate("aab");
-
     return result;
-
 };
-console.log(partition("aab"))
+console.log(partition("a"));
+
 // @lc code=end
 
