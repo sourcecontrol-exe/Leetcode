c0b912bc56c60602dd567583c2c985997131f34f
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-08 22:18:43 +0530

    leetcode problem

---
+++ 492.construct-the-rectangle.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=492 lang=javascript
+ *
+ * [492] Construct the Rectangle
+ */
+
+// @lc code=start
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function(area) {
+   for(var i =0; i<Math.sqrt(n)/2; i++){
+       console.log(i)
+   }
+};
+console.log(constructRectangle(37));
+// @lc code=end
+
