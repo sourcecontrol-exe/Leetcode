4f7e951f93fe363bac53638e24262205248bb60b
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-10 18:28:43 +0530

    leetcode problem

--- 218.the-skyline-problem.js
+++ 218.the-skyline-problem.js
@@ -9,8 +9,25 @@
  * @param {number[][]} buildings
  * @return {number[][]}
  */
-var getSkyline = function(buildings) {
+//left  => 2
+// right => 7
+// hight => 10
+
+var getSkyline = function (buildings) {
     
+    if(buildings.length==0) return [];
+
+    let PriorityQueue = [];
+
+    for(let building of buildings){
+        let [start,end,height] =  building;
+        PriorityQueue.push([start, 0-height]);
+        PriorityQueue.push([start, height]);
+    }
+    PriorityQueue.sort((a,b)=> a[0]==b[0]?a[1]-b[1]: a[0]-b[0]);
+    let result =[];
+    console.log(PriorityQueue);
 };
+console.log(getSkyline([[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]))
 // @lc code=end
 
