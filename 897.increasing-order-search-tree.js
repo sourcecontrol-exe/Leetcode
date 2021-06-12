2ea03df89d56ee32a9cc1e321f4623d20f277724
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-13 03:47:36 +0530

    leetcode problem

---
+++ 897.increasing-order-search-tree.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=897 lang=javascript
+ *
+ * [897] Increasing Order Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function (root) {
+	if (!root) return null;
+	increasingBST(root.left);
+	
+};
+// @lc code=end
+
