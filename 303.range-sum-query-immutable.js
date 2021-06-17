c36f456d25712612bc9186aad59ba66373ace97b
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-17 18:22:52 +0530

    leetcode problem

---
+++ 303.range-sum-query-immutable.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=303 lang=javascript
+ *
+ * [303] Range Sum Query - Immutable
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+	this.arr = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+
+	let sum = 0;
+	for (var i = left; i <= right; i++) {
+		if(this.arr[i]!==undefined) sum+= this.arr[i];
+	}
+	return sum
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
+
