93bb7144fa9e54a7ea50c30ae47c2aa6bc0b66d1
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-19 23:54:22 +0530

    leetcode problem

--- 48.rotate-image.js
+++ 48.rotate-image.js
@@ -17,9 +17,18 @@ var rotate = function (matrix) {
 	for (let i = 0; i < mid; i++) {
 		temp = matrix[i];
 		matrix[i] = matrix[matrix.length - 1 - i];
-		matrix[matrix.length-1-i] = temp;
+		matrix[matrix.length - 1 - i] = temp;
 	}
-	console.log(matrix);
+	for (let i = 0; i < matrix.length/2; i++) {
+		for (let j = i + 1; j < matrix[i].length; j++) {
+			if (matrix[i][j] && matrix[j][i]) {
+				let t = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = t
+			}
+		}
+	}
+	return matrix
 };
-console.log(rotate([[7,4,1],[8,5,2],[7,8,9]]));
+console.log(rotate([[1, 2, 3], [, 4, 5, 6], [7, 8, 9]]));
 // @lc code=end
