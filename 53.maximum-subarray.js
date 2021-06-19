ddb712fbcecf87e0efc07c36a855a5b07fe3d079
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-20 03:11:37 +0530

    leetcode problem

--- 53.maximum-subarray.js
+++ 53.maximum-subarray.js
@@ -10,14 +10,21 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
+	if (nums.length == 0) return 0;
 	let dp = new Array(nums.length).fill(0);
+
 	dp[0] = nums[0];
-	dp[1] = Math.max(dp[0]+dp[1], dp[0]);
-	for(let i = 2 ;i< nums.length ;i++){
-		dp[i] = Math.max(dp[i]+dp[i-1], dp[i-1]);
+
+	for (var i = 0; i < nums.length; i++) {
+		dp[i+1] = Math.max(nums[i] + dp[i], dp[i])
 	}
 	console.log(dp)
+	//return dp[dp.length-1]
 };
-console.log([-2,1,-1,4 ,-1,2,1,-5,4]);
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5]))
+/*0 0 0 0 0
+-2 1
+*/
+
 // @lc code=end
 
