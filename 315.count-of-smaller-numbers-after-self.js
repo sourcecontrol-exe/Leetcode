4160a1b0d2e01c9544b21c1191da935774a6b22d
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-19 23:12:53 +0530

    leetcode problem

--- 315.count-of-smaller-numbers-after-self.js
+++ 315.count-of-smaller-numbers-after-self.js
@@ -9,24 +9,63 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var countSmaller = function (nums) {
+var countSmaller2 = function (nums) {
 	let sorted = [], result = [];
 	for (var i = nums.length - 1; i >= 0; i--) {
 		let left = 0, right = sorted.length;
 		while (left < right) {
-			let mid = left + Math.floor((right-left)/2);
-			console.log(mid)
-			if(nums[i] > sorted[mid]){	
-				left = mid+1;
+			let mid = left + Math.floor((right - left) / 2);
+			if (nums[i] > sorted[mid]) {
+				left = mid + 1;
 			}
-			else{
+			else {
 				right = mid;
 			}
 		}
 		result.unshift(left);
-		sorted.splice(left,0,nums[i]);
+		sorted.splice(left, 0, nums[i]);
 	}
 	return result;
 };
+var countSmaller = function (nums) {
+	if (nums.length == 0 || !nums) return nums;
+
+    let inversion = new Array(nums.length).fill(0);
+    let map = nums.map((val, index)=>{
+        return {'val':val, 'index':index}
+    })
+
+    var merge = function(arr) {
+        if (arr.length == 1) {
+            return arr
+        }
+        let mid = Math.floor(arr.length/2)
+        let left = merge(arr.slice(0,mid));
+        let right = merge(arr.slice(mid));
+        
+        let li = 0, ri = 0, inversionCount = 0, sorted = [];
+        // compare numbers from left part to right part
+        while (li < left.length ) {
+            if (right[ri] && left[li].val > right[ri].val) {
+                // inversion found
+                inversionCount++;
+                sorted.push(right[ri++]);
+            } else {
+                // no inversions for this number (or right is exhausted)
+                // update its inversion count up to the current stack
+                inversion[left[li].index]+=inversionCount;
+                sorted.push(left[li++]);
+            }
+        }
+
+        // deal with left over right values and return
+        return [...sorted, ...right.slice(ri)];        
+    }
+
+    merge(map);
+    return inversion;
+
+}
+console.log(countSmaller([3, 4, 6, 3, 5]))
 // @lc code=end
 
