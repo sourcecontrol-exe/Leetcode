2b57bb7af93a0fbaebd82bed0b90716c96a7f101
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-23 01:44:08 +0530

    leetcode problem

---
+++ 91.decode-ways.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=91 lang=javascript
+ *
+ * [91] Decode Ways
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s) {
+
+	let set = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'])
+
+
+
+};
+console.log(numDecodings("asd"));
+/*
+226
+i=1
+
+
+*/
+// @lc code=end
+
