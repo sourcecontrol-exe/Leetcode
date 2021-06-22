4906676a02dd264830eac1083fac36cc823f522e
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-23 00:08:14 +0530

    leetcode problem

--- 746.min-cost-climbing-stairs.js
+++ 746.min-cost-climbing-stairs.js
@@ -9,13 +9,15 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
-    
-	let dp = new Array(nums.lenght+1).fill(0);
-	dp[0] = cost[0];
-	for(var i = 0; i<nums.length;i++){
-		dp[i+1] =  
-	}
-};
+var minCostClimbingStairs = function (cost) {
 
+	if (cost.length < 3) return Math.min(...cost);
+
+	
+
+	for (let i = 2; i < cost.length ; i++){
+		cost[i]+=Math.min(cost[i-2], cost[i-1]);
+	}
+	return Math.min(cost[cost.length-1], cost[cost.length-2]);
+};
 // @lc code=end
