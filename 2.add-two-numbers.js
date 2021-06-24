95405caf179bb1ca603048b91d6f16301a2ee720
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-24 11:18:46 +0530

    leetcode problem

--- 2.add-two-numbers.js
+++ 2.add-two-numbers.js
@@ -17,9 +17,31 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-	
-    
+var addTwoNumbers = function (l1, l2) {
+	let res = new ListNode(0);
+	let carry = 0;
+	let sum = 0;
+	let temp = res;
+	while (l1 || l2 || sum) {
+		if (l1 !== null) {
+			sum = sum + l1.val;
+			l1 = l1.next;
+		}
+		if (l2 !== null) {
+			sum = sum + l2.val;
+			l2 = l2.next;
+		}
+		if (sum >= 10) {
+			carry = 1;
+			sum = sum - 10;
+		}
+		temp.next = new ListNode(sum);
+		temp = temp.next;
+
+		sum = carry;
+		carry = 0;
+	}
+	return res.next;
+
 };
 // @lc code=end
-
