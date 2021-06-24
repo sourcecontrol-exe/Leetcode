220ff7a476df7d3fa69c7461b5da003994712d1d
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-24 08:43:55 +0530

    leetcode problem

--- 463.island-perimeter.js
+++ 463.island-perimeter.js
@@ -9,23 +9,20 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var islandPerimeter = function(grid) {
-   let perimeter = 0
-    for (let row = 0; row < nums.length; row++) {
-        for (let col = 0; col < nums[i].length; col++) {
-            if (nums[row][col] === 1) {
-                perimeter+=4;
+var islandPerimeter = function (grid) {
 
-		if (row > 0 && grid[row - 1][col]) perimeter--;
-		if (col > 0 && grid[row][col - 1]) perimeter--;
-		if (row < rows - 1 && grid[row + 1][col]) perimeter--;
-		
-
+    let perimeter = 0;
+    let neighbour = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if(grid[i][j] == 1){
+                perimeter++;
+                if(i< grid.length-1 && grid[i+1][j]) neighbour++;
+                if(j<grid[i].length-1 && grid[i][j+1])  neighbour++;
             }
         }
     }
-
-    return perimeter 
+    return 4*perimeter - 2*neighbour
 };
 // @lc code=end
 
