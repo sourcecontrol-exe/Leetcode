5db5b952cd22d7e3917bb6f89c7c956bfcd9307e
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-26 14:35:11 +0530

    leetcode problem

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var inorderTraversal = function(root, res=[]) {
+    if(!root) return res;
+    if(root.left) inorderTraversal(root.left, res);
+    res.push(root.val);
+    if(root.right) inorderTraversal(root.right,res);
+    return res;
+};
