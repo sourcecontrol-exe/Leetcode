4ee7924d476cf892f24dbe5d26159cc50c5e7493
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-27 19:15:45 +0530

    leetcode problem

---
+++ 416.partition-equal-subset-sum.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=416 lang=javascript
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums) {
+	let sum = 0;
+	for (var i = 0; i < nums.length; i++) {
+		sum += nums[i];
+	}
+	if (sum % 2 !== 0) return false;
+
+	let target = sum/2;
+	var dp = new Array(target + 1).fill(0);
+	for (var i = 0; i < nums.length; i++) {
+		for (var j = target; j >= nums[i]; j--) {
+			dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+			console.log(dp)
+		}
+	}
+	return dp[target] === target;
+
+};
+console.log(canPartition([1,2,5,5,11]));
+// @lc code=end
+
