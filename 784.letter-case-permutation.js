c280bcb32ef788b8bddd9408f8058bcd8f5d7e32
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-05 01:11:07 +0530

    leetcode problem

--- 784.letter-case-permutation.js
+++ 784.letter-case-permutation.js
@@ -9,23 +9,33 @@
  * @param {string} s
  * @return {string[]}
  */
-var letterCasePermutation = function (s, curr = '', result = [], index = 0) {
-	if(curr.length == s.length){
-		result.push(curr);
+var letterCasePermutation1 = function (s, curr = '', result = new Set(), index = 0) {
+	if (result.has(curr)) return;
+	if (index == s.length) {
+		result.add(curr);
 		return;
-	}	
-	if(s[index] == s[index].toUpperCase() ){
-		letterCasePermutation(s,curr+s[index].toLowerCase(),result, index+1)
 	}
-	else if(s[index] == s[index].toLowerCase()){
-		letterCasePermutation(s,curr+s[index].toUpperCase(),result,index+1);
+	if ((!isNaN(s[index]))) {
+		letterCasePermutation(s, curr + s[index], result, index + 1)
 	}
-	else {
-		letterCasePermutation(s,curr+s[index],result, index+1)
-	}
+	letterCasePermutation(s, curr + s[index].toLowerCase(), result, index + 1)
 
-	return result;
+	letterCasePermutation(s, curr + s[index].toUpperCase(), result, index + 1);
+
+	return [...result];
 };
-console.log(letterCasePermutation("a1b2"));
+var letterCasePermutation = function (S) {
+	const queue = [S];
+	for (let i = 0; i < S.length; i++) {
+		if (!isNaN(S[i])) continue;
+		const size = queue.length;
+		for (let j = 0; j < size; j++) {
+			const node = queue.shift();
+			queue.push(node.slice(0, i) + node[i].toLowerCase() + node.slice(i + 1));
+			queue.push(node.slice(0, i) + node[i].toUpperCase() + node.slice(i + 1));
+		}
+	}
+	return queue;
+};
 // @lc code=end
 
