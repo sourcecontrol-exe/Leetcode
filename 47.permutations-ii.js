3bb4611842e002e4d57154ff892dba1b82c35653
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-04 09:57:54 +0530

    leetcode problem

---
+++ 47.permutations-ii.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=47 lang=javascript
+ *
+ * [47] Permutations II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+   nums= nums.toString.join(",");
+	console.log(nums)
+};
+console.log(permuteUnique([1,2,3]))
+// @lc code=end
+
