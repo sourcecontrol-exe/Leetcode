f698001ebf61f4ed4a56f878ee7a6139e4a94d65
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-05 01:55:35 +0530

    leetcode problem

---
+++ 401.binary-watch.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=401 lang=javascript
+ *
+ * [401] Binary Watch
+ */
+
+// @lc code=start
+/**
+ * @param {number} turnedOn
+ * @return {string[]}
+ */
+var readBinaryWatch = function(turnedOn) {
+   let time = "0000000000";
+   let window = new Array(turnedOn).fill(1).join("")
+   console.log(window);
+   for(let i = 0 ;i< time.length- turnedOn;i++){
+	   
+   }
+
+};
+console.log(readBinaryWatch(3));
+// @lc code=end
+
