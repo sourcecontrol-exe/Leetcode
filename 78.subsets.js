cc68abb67f1bce7ad4e045dfbeba76341920e717
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-05 01:25:23 +0530

    leetcode problem

---
+++ 78.subsets.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=78 lang=javascript
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+	let res = [[]];
+
+	for (let i = 0;i<nums.length ;i++){
+		res.forEach(ele=>{
+			res.push([...ele, nums[i]])
+		})
+	}
+	console.log(res)
+};
+// @lc code=end
+
