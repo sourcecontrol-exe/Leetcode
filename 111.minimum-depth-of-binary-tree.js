a69f2fb376b6979b07eea8d0ecf672fef7a6614e
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-07 12:50:58 +0530

    leetcode problem

--- 111.minimum-depth-of-binary-tree.js
+++ 111.minimum-depth-of-binary-tree.js
@@ -18,7 +18,23 @@
  * @return {number}
  */
 var minDepth = function (root) {
-	 
+	if(!root) return 0;
+
+	let min = Number.MAX_SAFE_INTEGER;
+	function helper(root, depth=0) {
+	
+		if(!root) return
+		if(!root.left   && !root.right) {
+			min = Math.min(min, depth+1);
+			return;
+		}
+		helper(root.left, depth+1);
+		helper(root.right, depth+1);
+		
+	}
+	helper(root);
+	return min;
+
 };
 // @lc code=end
 
