92ce38e5249e297f633adda2dc5a556a4c123294
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-08 23:38:36 +0530

    leetcode problem

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -9,8 +9,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    
+var productExceptSelf = function (nums) {
+	let leftArr = [1];
+	let leftmul = 1;
+
+	for (let i = 0; i < nums.length; i++) {
+		leftArr[i] = leftmul;
+		leftmul *= nums[i];
+	}
+	let rightArr = new Array(nums.length);
+	let rightMul = 1;
+
+	for (let i = nums.length - 1; i >-1 ;i--){
+		rightArr[i] = rightMul;
+		rightMul*=nums[i];
+		rightArr[i]*=leftArr[i]
+	}
+	return rightArr;
 };
 // @lc code=end
 
