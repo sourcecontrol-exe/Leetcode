db0078491a25197cf50a77b5de31261acd569ce8
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-09 23:33:22 +0530

    leetcode problem

--- 239.sliding-window-maximum.js
+++ 239.sliding-window-maximum.js
@@ -10,20 +10,58 @@
  * @param {number} k
  * @return {number[]}
  */
+/*
+max = 3 index = 1;
+
+
+*/
 var maxSlidingWindow = function (nums, k) {
-	if(k==1) return nums;
-	let currntMax = Number.MIN_SAFE_INTEGER;
-	for (let j = 0; j < k; j++) {
-		if(nums[j]> currntMax) currntMax=nums[j];
-	}
-	let res = [currntMax];
 	
-	for (var i = k; i < nums.length; i++) {
-		if (nums[i] > currntMax) currntMax = nums[i];
-		res.push(currntMax);
+	// Iterate rate from 0 to K in an array and mark mark Maxelement and 
+	// index of the max element
+
+	if(nums.length ==1 || k ==1) return nums;
+	let currmax = Number.MIN_SAFE_INTEGER;
+	let maxIndex = 0;
+	for (let i = 0; i < k; i++) {
+		if (nums[i] > currmax) {
+			currmax = nums[i];
+			maxIndex = i
+		}
+	}
+	// push th max element in the result array
+	let res = [currmax];
+	
+	// iterate from index 1 since we have already calculated our first max for First 
+	// 0 to K elements of nums array
+
+	for (let i = 1; i <= nums.length - k; i++) {
+		
+		// check if the index of max element is less than the current index value 
+		// which suggests we need to find new max and new index of the max element
+		
+		if (maxIndex < i) {
+			currmax = Number.MIN_SAFE_INTEGER;
+			for (let j = i; j < i + k; j++) {
+				if (currmax < nums[j]) {
+					currmax = nums[j];
+					maxIndex = j;
+				}
+			}
+		}
+
+		// second check to see if the incomming element in the window is greater than the current Max
+		// we have 
+
+		if (currmax < nums[i + k - 1]) {
+			currmax = nums[i + k - 1];
+			maxIndex = i;
+		}
+		res.push(currmax);
 	}
 	return res;
+
 };
-console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7],2));
-//3 3 -1 5 5 6 7
+// console.log(maxSlidingWindow([4,-2], 2));
+// 5 5
 // @lc code=end
