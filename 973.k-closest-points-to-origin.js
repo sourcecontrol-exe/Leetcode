5d31535b0531c76de699de7eb4a33135cf2ba6ab
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-11 15:47:06 +0530

    leetcode problem

--- 973.k-closest-points-to-origin.js
+++ 973.k-closest-points-to-origin.js
@@ -10,23 +10,40 @@
  * @param {number} k
  * @return {number[][]}
  */
+
 var kClosest = function (points, k) {
 
-	let res = {}
-	let arr = []
-	for (let [x, y] of points) {
-		arr.push([x, y]);
-
-		if(arr.length > k){
-			console.log(arr)
-			[[5,5],[]].sort((a,b)=> {
-				Math.pow((Math.pow(a[0],2)+Math.pow(a[1],2)),0.5) - Math.pow((Math.pow(b[0],2)+Math.pow(b[1],2)),0.5)
-			})
-			arr.pop()
-		}
-	}
-	console.log(arr)
+	points.sort(([a,b],[c,d])=> (Math.pow(a,2)+Math.pow(b,2)) - (Math.pow(c,2)+Math.pow(d,2)))
+	return points.slice(0,k);
 };
-console.log(kClosest([[1, 3], [-2, 2]], 1))
+
+var kClosest1 = function (points, k) {
+
+	let arr = []
+	let isAdded = false;
+	for(let [x,y] of points){
+		if(!arr.length){
+			arr.push([x,y])
+			continue;
+		}
+		for(let j =0;j< arr.length;j++){
+			let items = arr[j];
+			
+			if(Math.sqrt(Math.pow(x,2)+Math.pow(y,2)) <= Math.sqrt(Math.pow(items[0],2)+Math.pow(items[1],2))){
+				arr.splice(j,0,[x,y]);
+				isAdded = true
+				break;
+			}
+		}
+		if(!isAdded){
+			arr.push([x,y]);
+			
+		}
+		isAdded = false;
+		if(arr.length>k) arr.pop();
+	}
+	return arr
+};
+//console.log(kClosest([[-5,4],[-3,2],[0,1],[-3,7],[-2,0],[-4,-6],[0,-5]], 6))
 // @lc code=end
 
