ba0bd589e901e68753f1de635f79172add9dd642
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-13 01:29:14 +0530

    leetcode problem

--- gfg/longestKUniquesubStr.js
+++ gfg/longestKUniquesubStr.js
@@ -1,13 +1,8 @@
 module.exports = {
-	sub: (s, k)=>{
-		let first = 0, last = s.length;
-		let max = Number.MIN_SAFE_INTEGER;
-		function helper(){
-			while(first<last){
-				let s1 = s.splice(first, last)
-			}
-		}
-		helper()
-		return max;
+	sub: (s, k) => {
+		
 	}
 }
+/*
+* take a window of K elemts while 
+*/
