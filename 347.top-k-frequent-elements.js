406b0b68f53b3f78b9366db186b02b57b4835ea7
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-13 18:10:54 +0530

    leetcode problem

---
+++ 347.top-k-frequent-elements.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=347 lang=javascript
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+ 
+	let obj = {}
+
+	for(let items of nums){
+		if(!obj[items]) obj[items]= 0;
+		obj[items]++
+	}
+
+	let res = Object.keys(obj);
+
+	res.sort((a,b)=> obj[b]-obj[a]);
+
+	let ans = res.map(ele=> parseInt(ele))
+	return ans.slice(0,k)
+
+};
+// @lc code=end
+
