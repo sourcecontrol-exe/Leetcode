81258a9171a1c34e66117141326213741b354500
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-13 18:11:51 +0530

    leetcode problem

--- 347.top-k-frequent-elements.js
+++ 347.top-k-frequent-elements.js
@@ -24,7 +24,8 @@ var topKFrequent = function(nums, k) {
 	res.sort((a,b)=> obj[b]-obj[a]);
 
 	let ans = res.map(ele=> parseInt(ele))
-	return ans.slice(0,k)
+
+	return res.slice(0,k).map(ele=> parseInt(ele))
 
 };
 // @lc code=end
