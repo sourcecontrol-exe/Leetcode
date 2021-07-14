86316aea6bea1fcb2ef2c7bc2309736a63b3dfd6
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-14 19:00:37 +0530

    leetcode problem

--- 406.queue-reconstruction-by-height.js
+++ 406.queue-reconstruction-by-height.js
@@ -10,7 +10,16 @@
  * @return {number[][]}
  */
 var reconstructQueue = function(people) {
+	people.sort((a,b)=> a[0]-b[0]);
+
+	console.log(people);
+	// for(let i = people.length-1 ;i>=0;i--){
+	// 	let ele = people.splice(i,1);
+	// 	people.splice(i+ele[1],0,[...ele]);
+	// }
+	//return people
     
 };
+console.log(reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]))
 // @lc code=end
 
