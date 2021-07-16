a2ed680b6f7175ba6d3447ad536ea4b9915ffb70
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-17 02:07:33 +0530

    leetcode problem

---
+++ 670.maximum-swap.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=670 lang=javascript
+ *
+ * [670] Maximum Swap
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function(num) {
+	
+	let arr = num.toString().split().sort((a,b)=> b-a);
+
+	num = num.toString();
+
+	for(let item of arr){
+
+	}
+	console.log(nums)
+};
+console.log(maximumSwap(4575))
+// @lc code=end
+
+/*
+34589
+9 8 5 4 3
+ */
