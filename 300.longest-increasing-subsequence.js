7c9aeed5ce47c69b871d7c498e218645e691fe30
swetabh <swetabh.subham@gmail.com>
2021-07-17 16:37:47 +0530

    new

--- 300.longest-increasing-subsequence.js
+++ 300.longest-increasing-subsequence.js
@@ -9,15 +9,32 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums, i) {
-	var lis = [];
-	for (var i = 0; i < nums.length; i++) {
-		lis.push(1);
-		for (var j = 0; j < i; j++) {
-			if (nums[j] < nums[i]) lis[i] = Math.max(lis[i], lis[j] + 1);
+var lengthOfLIS = function (nums) {
+	let dp = [nums[0]];
+
+	function binaryInsert(val){
+		let low =0;
+		let high = dp.length-1;
+		while(low<high){
+			let mid = low + Math.floor((high-low)/2);
+			if(val > dp[mid]){
+				low = mid+1;
+			}
+			else{
+				high = mid;
+			}
+		}
+		return low;
+	}
+
+	for(let item of nums){
+		if(item > dp[dp.length-1]) dp.push(item);
+		else{
+			let index = binaryInsert(item)
+			dp[index] = item;
 		}
 	}
-	return nums.length? Math.max(...lis) : 0;
+	return dp.length;
 };
 // @lc code=end
 
