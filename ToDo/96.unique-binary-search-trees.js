8631fed0e3e69b4d766e1d8965921061693cee24
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-20 21:32:07 +0530

    leetcode problem

--- ToDo/96.unique-binary-search-trees.js
+++ ToDo/96.unique-binary-search-trees.js
@@ -10,7 +10,14 @@
  * @return {number}
  */
 var numTrees = function(n) {
-    
+   function fibonacci(n, set={}){
+	   if(n in set) return set[n]
+	   if(n<=2) return n;
+	   set[n]= n*fibonacci(n-1);
+	   return set[n]
+   } 
+ 
+  return fibonacci( 2 * n ) / ( fibonacci( n + 1 ) * fibonacci( n ) );
 };
 // @lc code=end
 
