b3af1ad099b7b6a04d877be466a89cdb52cb108d
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-24 00:17:53 +0530

    leetcode problem

--- 0-1_bounded_knapsack.js
+++ 0-1_bounded_knapsack.js
@@ -1,14 +1,17 @@
-function knapsack(target, weights, values, index = weights.length-1){
-	
+function knapsack(target, weights, values, index = weights.length-1,set = {}){
+
+	let key = target
+	if(key in set) return set[key];
 	if(target< 0){
 		return Number.MIN_SAFE_INTEGER
 	}
 	if(target == 0 || index < 0){
 		return 0;
 	}
-	let included = values[index] + knapsack(target - weights[index], weights , values, index-1);
-	let excluded = knapsack(target, weights, values, index-1);
+	let included = values[index] + knapsack(target - weights[index], weights , values, index-1, set);
+	let excluded = knapsack(target, weights, values, index-1, set);
 	
-	return Math.max(included, excluded);
+	set[key] =  Math.max(included, excluded);
+	return set[key]
 }
-console.log(knapSack(15,[4, 2, 1, 10, 2],[12, 2, 1, 4, 1]))
+console.log(knapsack(15,[4, 2, 1, 10, 2],[]))
