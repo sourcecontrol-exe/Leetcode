f5ebc85d5ac5378a96c51c88183d730efdc98f4d
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-24 01:14:16 +0530

    leetcode problem

---
+++ 322.coin-change.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=322 lang=javascript
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+	if (amount == 0) return 0;
+	let res = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER)
+	for (let i = 0; i <= amount; i++) {
+		for (let j = 0; j < coins.length; j++) {
+			if (coins[j] <= i) {
+				res[i] = Math.min(res[i], res[i - coins[j]] + 1)
+			}
+		}
+	}
+
+	console.log(res)
+};
+console.log(coinChange([1, 2, 5], 11))
+// @lc code=end
+
