46036f92950aed18e9f9da534f62212b5c4f597c
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-25 01:02:08 +0530

    leetcode problem

--- 416.partition-equal-subset-sum.js
+++ 416.partition-equal-subset-sum.js
@@ -10,21 +10,29 @@
  * @return {boolean}
  */
 var canPartition = function (nums) {
-	let sum = 0;
-	for (var i = 0; i < nums.length; i++) {
-		sum += nums[i];
-	}
-	if (sum % 2 !== 0) return false;
 
-	let target = sum/2;
-	var dp = new Array(target + 1).fill(0);
-	for (var i = 0; i < nums.length; i++) {
-		for (var j = target; j >= nums[i]; j--) {
-			dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
-		}
-	}
-	return dp[target] === target;
-};
+	let total = 0
+	nums.map(ele => total += ele)
 
+	if (total % 2 !== 0) return false;
+
+	function permute(sum1, sum2=0, i = nums.length-1, set = {}){
+		let key = sum1+""+sum2
+
+		if(key in set) return set[key];
+		
+		if(sum1 == sum2) return true
+
+		if(sum1<sum2 || i < 0) return false;
+
+		
+
+		let a = permute(sum1 - nums[i], sum2 + nums[i], i-1, set)
+		let b = permute(sum1,sum2, i-1, set)
+		set[key] = a || b
+		return set[key]
+	}
+	return permute(total)
+}
 // @lc code=end
 
