af8048816c67e7d43dea54d273ec620d795e75ab
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-27 01:22:11 +0530

    leetcode problem

--- 1235.maximum-profit-in-job-scheduling.js
+++ 1235.maximum-profit-in-job-scheduling.js
@@ -16,8 +16,8 @@ var jobScheduling = function (startTime, endTime, profit) {
 	class node {
 		constructor(start, end, profit) {
 			this.start = start,
-			this.end = end,
-			this.profit = profit
+				this.end = end,
+				this.profit = profit
 		}
 	}
 
@@ -26,26 +26,33 @@ var jobScheduling = function (startTime, endTime, profit) {
 	let tasks = []
 
 	for (let i = 0; i < n; i++) {
-		tasks.push(new node(startTime[i],endTime[i], profit[i]))
+		tasks.push(new node(startTime[i], endTime[i], profit[i]))
 	}
 
-	tasks.sort((a,b) => a.startTime - b.startTime);
+	tasks.sort((a, b) => a.startTime == b.startTime ? a.endTime - b.endTime : a.startTime - b.startTime);
 
-	let dp = new Array(n+1).fill(0);
+	let dp = new Array(n + 1).fill(0);
 
-	function findMax(index){
-		if(index>=tasks.length) return 0;
-		
-		let curr = index;
+	let profitTracker = new Array(n+1).fill(0)
 
-		while(curr<tasks.length && tasks[curr].startTime < tasks[index].endTime) curr++;
-
-		let take = tasks[index].profit + findMax(curr)
-		let exclude = findMax(index+1);
-		return Math.max(take, exclude);
+	function bs(index){
+		let low = 0;
+		let high = tasks.length;
+		while(low< high){
+			let mid = Math.floor((low+high)/2);
+			if(tasks[mid].startTime >=tasks[index].endTime){
+				low = mid;
+			}
+			else{
+				high = mid-1;
+			}
+		}
+		return low
 	}
-
-	return findMax(0)
+	for(let i = tasks.length-1; i<=0 ; i--){
+		let index = bs(i);
+		console.log(index);
+	}
 };
 console.log(jobScheduling([1,2,3,3],[3,4,5,6],[50,10,40,70]))
 // @lc code=end
