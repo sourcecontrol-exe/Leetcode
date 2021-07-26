a6dc755981e10d146afaad8bc09fd783f23517db
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-27 00:17:57 +0530

    leetcode problem

--- 1235.maximum-profit-in-job-scheduling.js
+++ 1235.maximum-profit-in-job-scheduling.js
@@ -11,8 +11,42 @@
  * @param {number[]} profit
  * @return {number}
  */
-var jobScheduling = function(startTime, endTime, profit) {
-    
+var jobScheduling = function (startTime, endTime, profit) {
+
+	class node {
+		constructor(start, end, profit) {
+			this.start = start,
+			this.end = end,
+			this.profit = profit
+		}
+	}
+
+	let n = startTime.length
+
+	let tasks = []
+
+	for (let i = 0; i < n; i++) {
+		tasks.push(new node(startTime[i],endTime[i], profit[i]))
+	}
+
+	tasks.sort((a,b) => a.startTime - b.startTime);
+
+	let dp = new Array(n+1).fill(0);
+
+	function findMax(index){
+		if(index>=tasks.length) return 0;
+		
+		let curr = index;
+
+		while(curr<tasks.length && tasks[curr].startTime < tasks[index].endTime) curr++;
+
+		let take = tasks[index].profit + findMax(curr)
+		let exclude = findMax(index+1);
+		return Math.max(take, exclude);
+	}
+
+	return findMax(0)
 };
+console.log(jobScheduling([1,2,3,3],[3,4,5,6],[50,10,40,70]))
 // @lc code=end
 
