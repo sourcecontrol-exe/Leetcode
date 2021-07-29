f7fa595679c4f1a4daf1c64f43ba2d487b07eb81
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-29 18:29:57 +0530

    leetcode problem

---
+++ spoj/apples.py
@@ -0,0 +1,18 @@
+import math
+import sys
+def UBK(sum, index, arr):
+        
+        if(sum<0 or index<0): return 0
+        
+        if(sum==0): return arr[index-1]
+        
+        if(arr[index-1] == -1): return UBK(sum,index-1,arr)
+        
+        include =  UBK(sum-index, index, arr) + arr[index-1]
+        
+        exclude = UBK(sum, index-1,arr)
+        
+        return min(include, exclude)
+
+ans = UBK(5, 5,[-1,-1,4,5,-1])
+print(ans)
--- spoj/primeGenerator.py
+++ spoj/primeGenerator.py
@@ -1,19 +1,45 @@
+
 import math
+ 
 
+def fillPrimes(chprime, high):
+ 
+    ck = [True]*(high+1)
+ 
+    l = int(math.sqrt(high))
+    for i in range(2, l+1):
+        if ck[i]:
+            for j in range(i*i, l+1, i):
+                ck[j] = False
+                 
+                 
+ 
+    for k in range(2, l+1):
+        if ck[k]:
+            chprime.append(k)
 
-def check(n):
-    if n == 1:
-        return False
-    for x in range(2, (int)(math.sqrt(n))+1):
-        if n % x == 0:
-            return False
-    return True
-
-
+ 
 test = int(input())
 for _ in range(test):
-    [n, k] = [int(x) for x in input().split()]
-    arr = [True]*(k-n)
-    for l in range(n, k+1):
-        if(check(l)):
-            print(l)
+    [low,high] = list(map(int,input().split()))
+    chprime = list()
+    fillPrimes(chprime, high)
+
+    prime = [True] * (high-low + 1)
+
+    for i in chprime:
+        lower = (low//i)
+
+        if lower <= 1:
+            lower = i+i
+        elif (low % i) != 0:
+            lower = (lower * i) + i
+        else:
+            lower = lower*i
+        for j in range(lower, high+1, i):
+            prime[j-low] = False
+             
+             
+    for k in range(low, high + 1):
+            if prime[k-low]:
+                print(k)
