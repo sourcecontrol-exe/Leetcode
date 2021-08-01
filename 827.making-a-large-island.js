7424f109bd90353647c9474fa98a5e3e18ce74d4
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-01 23:35:58 +0530

    leetcode problem

--- 827.making-a-large-island.js
+++ 827.making-a-large-island.js
@@ -10,45 +10,50 @@
  * @return {number}
  */
 var largestIsland = function (grid) {
-	let g = grid
-	let n = grid.length
-	let m = g[0].length
-	let res = 0;
-	let sizes = [0, 0]
 
-	const getColor = (i, j) => {
-		return (i < 0 || j < 0 || i >= n || j >= m) ? 0 : g[i][j];
-	};
+	let islandID = 2;
 
-	const paint = (i, j, color) => {
-		if (i < 0 || j < 0 || i >= n || j >= m || g[i][j] !== 1) return 0
-		g[i][j] = color;
-		return 1 + paint(i + 1, j, color) + paint(i - 1, j, color) + paint(i, j + 1, color) + paint(i, j - 1, color);
-	};
+	let dfs = (i, j) => {
+		if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] !== 1) return 0
+		grid[i][j] = islandID
+		return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+	}
 
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < m; j++) {
-			if (g[i][j] == 1) {
-				let size = paint(i, j, sizes.length);
-				sizes.push(size);
+	let islandCount = 0;
+	let max = 0;
+	let map = new Map() // key is the island id and value is the total island count for that identifier
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[0].length; j++) {
+			if (grid[i][j] == 1) {
+				islandCount = dfs(i, j)
+				map.set(islandID++, islandCount)
 			}
 		}
 	}
-	console.log(g)
-	// for (let i = 0; i < n; i++) {
-	// 	for (let j = 0; j < m; j++) {
-	// 		if (g[i][j] == 0) {
-	// 			let se = new Set([getColor(i + 1, j), getColor(i - 1, j), getColor(i, j + 1), getColor(i, j - 1)]);
-	// 			console.log(se)
-	// 			let newSize = 1;
-	// 			for (const color of se) {
-	// 				newSize += sizes[color];
-	// 			}
-	// 			res = Math.max(res, newSize);
-	// 		}
-	// 	}
-	// }
-	return res == 0 ? n * m : res;
+
+	let sum  = 0
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[0].length; j++) {
+			if(grid[i][j] == 0){
+				let set = new Set()
+				sum = 0;
+				if(i>0 && grid[i-1][j] > 0) set.add(grid[i-1][j]);
+				if(i<grid.length-1 && grid[i+1][j] !== 0) set.add(grid[i+1][j]);
+				if(j>0 && grid[i][j-1] > 0) set.add(grid[i][j-1]);
+				if(j<grid[0].length-1 && grid[i][j+1] !== 0) set.add(grid[i][j+1]);
+				for(let item of [...set]){
+					sum+=map.get(item)
+				}
+				max = Math.max(max, sum+1);
+			}
+		}
+	}
+	if(max == 0 && map.size>0){
+		return map.values().next().value
+	}
+	return max
+
 };
 // @lc code=end
 
