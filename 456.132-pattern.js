e7e5daa65448f25ed9e584f2a2c50cce0e90d195
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-02 01:39:02 +0530

    leetcode problem

--- 456.132-pattern.js
+++ 456.132-pattern.js
@@ -13,15 +13,15 @@
 var find132pattern = function (nums) {
 	let s3 = Number.MIN_SAFE_INTEGER;
 	let stack = [];
-
 	for (let i = nums.length - 1; i >= 0; i--) {
 		if (nums[i] < s3) return true;
-		while (stack.length  > 0 && nums[i] > stack[stack.length-1]){
+		while (stack.length  > 1 && nums[i] > stack[stack.length-1]){
 			s3 = stack.pop();
 		}
 		stack.push(nums[i]);
 	}
 	return false;
 }
+console.log(find132pattern([3,1,4,2]));
 // @lc code=end
 
