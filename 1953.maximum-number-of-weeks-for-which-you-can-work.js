1399d81aab9f9a0ef7f75350cbb7fd662ed20cb2
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-05 06:25:07 +0530

    leetcode problem

---
+++ 1953.maximum-number-of-weeks-for-which-you-can-work.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1953 lang=javascript
+ *
+ * [1953] Maximum Number of Weeks for Which You Can Work
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} milestones
+ * @return {number}
+ */
+var numberOfWeeks = function(milestones) {
+    
+};
+// @lc code=end
+
