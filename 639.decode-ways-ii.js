fc524ec9ca38e204813012af74ee29f156a56ea1
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-07 23:10:04 +0530

    leetcode problem

---
+++ 639.decode-ways-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=639 lang=javascript
+ *
+ * [639] Decode Ways II
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    
+};
+// @lc code=end
+
--- 91.decode-ways.js
+++ 91.decode-ways.js
@@ -10,14 +10,12 @@
  * @return {number}
  */
 
-var numDecodings = function (s, i=0, memo = {}) {
+var numDecodings = function (s, i=0) {
 
-	if(i in memo) return memo[i];
-	if(s[i]==="0") return 0;
+	if(s[i]=="0") return 0;
+
 	if(i>= s.length-1) return 1;
-	
-	memo[i]= numDecodings(s,i+1, memo)+(s[i]+s[i+1]<27? numDecodings(s,i+2,memo):0)
-	return memo[i];
+	return numDecodings(s, i+1) + (s[i]+s[i+1] < 27 ? numDecodings(s,i+2) : 0);
 };
 /*
 226
