48572ff6ea83ce89a2e4281b04c363425e6f4108
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-09 20:53:51 +0530

    leetcode problem

--- codechef/augLong/problemdifficult.py
+++ codechef/augLong/problemdifficult.py
@@ -2,4 +2,6 @@ t = int(input())
 for _ in range(t):
         a = list(map(int, input().split()))
         d = list(set(a))
-        print(len(d)//2)
+        if(len(d) == 1): print(0)
+        elif(len(d) == 2 or len(d) == 3): print(1)
+        else: print(2)
