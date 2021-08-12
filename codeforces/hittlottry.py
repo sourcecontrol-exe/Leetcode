456a2d42798d0e4a6163507e366cb618c97de40f
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-13 01:58:07 +0530

    leetcode problem

--- codeforces/hittlottry.py
+++ codeforces/hittlottry.py
@@ -1,2 +1,8 @@
-arr = [1, 5, 10, 20, 100]
+
+arr = [100, 20, 10, 5, 1]
 target = int(input())
+ans = 0
+for i in range(len(arr)):
+       ans += target//arr[i]
+       target = target % arr[i]
+print(ans)
