b710d0c3a2de88cdbd4f958a52609d3258004cf4
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-15 20:24:48 +0530

    leetcode problem

--- 55.jump-game.js
+++ 55.jump-game.js
@@ -9,13 +9,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+/* 
+Greedy Approach
+ */
 var canJump = function (nums) {
-	let i =0; 
-	for(let reach = 0; i<=reach && i<nums.length;i++){
-		reach = Math.max(reach, nums[i]+i)
+	let n = nums.length, max = 0;
+	for (let i = 0; i < n; i++) {
+		if (max < i) return false;
+		max = Math.max(max, nums[i] + i);
 	}
-	return i==nums.length
+	return true;
 };
+/* Dynmaic Programming Approach 
+*/
+
+
 // console.log(canJump([2, 3, 1, 1, 4]))
 // @lc code=end
 
