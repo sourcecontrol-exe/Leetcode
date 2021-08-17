1992155f557bf3eee54e7bbb7ae9c2e2de666ad2
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-17 11:26:44 +0530

    leetcode problem

--- 987.vertical-order-traversal-of-a-binary-tree.js
+++ 987.vertical-order-traversal-of-a-binary-tree.js
@@ -18,41 +18,27 @@
  * @return {number[][]}
  */
 var verticalTraversal = function (root) {
-
-	let queue = [[root, 0]];
+	let queue = [[root, 0]]
 	let res = {}
 
-	while (queue.length) {
-
+	while(queue.length){
 		let n = queue.length;
 		let temp = {}
+		for(let i =0; i<n;i++){
+			let [node, index] = queue.shift()
+			if(!temp[index]) temp[index] = []
+			temp[index].push(node.val);
 
-		for (let i = 0; i < n; i++) {
-
-			let [node, index] = queue.shift();
-
-			if (!temp[index]) temp[index] = [];
-
-			temp[index].push(node.val)
-
-			if (node.right) queue.push([node.right, index + 1]);
-
-			if (node.left) queue.push([node.left, index - 1]);
-
+			if(node.left) queue.push([node.left, index-1])
+			if(node.right) queue.push([node.right, index+1])
 		}
-		for(let key in temp){
+		for(let keys in temp){
 			temp[key].sort((a,b)=> a-b);
-			if(res[key]){
-				
-				res[key] = [...res[key],...temp[key]]
-			}
-			else{
-				res[key] = temp[key]
-			}
+			if(res[key]) res[key] = [...res[key], ...temp[key]]
+			else res[key] = temp[key]
 		}
 	}
 
-	let ans = []
 	Object.entries(res).sort((a, b) => parseInt(a[0] - parseInt(b[0]))).map(ele=> ans.push(ele[1]))
 	return ans
 };
