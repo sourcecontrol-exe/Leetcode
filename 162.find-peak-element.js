89f621a87e5cbb95a26e6e08b025870571fec48b
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-20 03:52:54 +0530

    leetcode problem

--- 162.find-peak-element.js
+++ 162.find-peak-element.js
@@ -15,10 +15,11 @@ var findPeakElement = function(nums) {
 
 	while(left<right){
 		let mid = left+Math.floor((right-left)/2);
-		if(nums[right] >nums[left]) left = mid+1
-		else right = mid-1
+		if(nums[mid] > nums[mid+1]) right = mid
+		else left= mid+1
 	}
-	return nums[left]
+	return left
 };
+console.log(findPeakElement([1,2,3,1]))
 // @lc code=end
 
