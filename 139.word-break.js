1524e8ce016beb4e5495e2fccc9208f168a1689d
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-26 22:25:01 +0530

    leetcode problem

--- 139.word-break.js
+++ 139.word-break.js
@@ -11,7 +11,23 @@
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-    
+	 
+	let set = new Set (wordDict);
+	let map = {}
+	function helper(s,map){
+		if(s in map) return map[s];
+		if(s.length == 0) return true;
+		for(let i = 1; i<=s.length;i++){
+			if(set.has(s.slice(0,i)) && helper(s.slice(i), map)) {
+				map[s] = true
+				return true
+			}
+		}
+		map[s] = false
+		return false
+	}
+
+	return helper(s, map);
 };
 // @lc code=end
 
