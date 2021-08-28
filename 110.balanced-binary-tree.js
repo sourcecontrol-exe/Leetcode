bb7ad2143c0a48f0babc62cfc7bc6b4973a66d9d
Swetabh Subham <swetabh.subham@gmail.com>
2021-08-29 02:20:27 +0530

    leetcode problem

---
+++ 110.balanced-binary-tree.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=110 lang=javascript
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+
+	function dfs(root){
+		if(!root) return  0
+		let left = 1 + dfs(root.left)
+		let right = 1 + dfs(root.right);
+		if(Math.abs(left- right) >1) return Infinity
+		return Math.max(left, right);		
+	}
+
+	return dfs(root) === Infinity ? false : true
+};
+// @lc code=end
+
