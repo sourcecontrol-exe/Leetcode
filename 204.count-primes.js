c4ab8c44e810d13427cef8988c0b824fbdd176f5
Swetabh Shubham <swetabhshubham@Admins-MacBook-Pro.local>
2021-09-17 13:36:37 +0530

    leetcode problem

---
+++ 204.count-primes.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=204 lang=javascript
+ *
+ * [204] Count Primes
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+
+	let seen = new Uint8Array(n);
+	let ans = 0;
+	for (let i = 2; i < n; i++) {
+		if(seen[i]) continue;
+		ans++;
+		for (let j = i * i; j < n ; j+= i){
+			seen[j] = 1;
+		}
+	}
+	return ans;
+}
+// @lc code=end
